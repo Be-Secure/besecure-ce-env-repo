13d65b4a945579c3d3eaf4f7e6b75fb0312a7490
asa1997 <arunsureshampadath@gmail.com>
2021-05-03 12:56:32 +0530

    Added env1

---
+++ besman-env1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function __besman_install_env1
+{
+    echo "Installing env1.."
+    echo "Environment env1 installed successfully."
+}
+
+function __besman_uninstall_env1
+{
+    echo "Uninstalling env1.."
+    echo "Environment env1 uninstalled successfully."
+
+}
