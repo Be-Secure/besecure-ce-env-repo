4124e8b69ab371ae1c771ac8b15618d598e5b5d0
asa1997 <arunsureshampadath@gmail.com>
2022-06-15 17:14:10 +0530

    Updated install function

--- besman-lf-java-RT.sh
+++ besman-lf-java-RT.sh
@@ -2,42 +2,15 @@
 
 function __besman_install_lf-java-RT
 {
-    # local vm_repo requirements_yml vm_repo_dir environment version roles_dir
 
-    # environment=$1
-    # version=$2
-    # vm_repo=oah-bes-vm
-    # vm_repo_url=https://github.com/$BESMAN_NAMESPACE/$vm_repo
-    # vm_repo_dir=$BESMAN/envs/besman-$environment/$version/$vm_repo
-
-    # [[ -z $vm_repo_dir ]] && git clone $vm_repo_url $vm_repo_dir # Skips cloning if the folder oah-bes-vm is present
-
-    # requirements_yml=$vm_repo_dir/provisioning/requirements.yml
-
-    # roles_dir=$vm_repo_dir/data/roles
-
-    # ansible-galaxy install -r $requirements_yml -p $roles_dir
-
-    # ansible-playbook $vm_repo_dir/provisioning/oah-install.yml --tags "java"
-
-    # local roles
-
-    # roles=("threatdragon" "java" "maven")
-
-    # for i in "$roles"
-    # do
-    #     __besman_gh_clone "$BESMAN_NAMESPACE" "ansible-role-oah-$i" "$HOME"
-    # done
-
-    cat <<EOF >> $HOME/requirements.yml
-    ---
-    - src: https://github.com/asa1997/threatdragon
-    - src: https://github.com/asa1997/java
-    - src: https://github.com/asa1997/maven
-
+    cat <<EOF >> $HOME/tmp/requirements.yml
+---
+- src: https://github.com/asa1997/ansible-role-oah-threatdragon
+- src: https://github.com/asa1997/ansible-role-oah-java
+- src: https://github.com/asa1997/ansible-role-oah-maven
 EOF
 
-    __besman_ansible_galaxy_install_from_requirements "$HOME"
-    __besman_ansible_playbook_extra_vars "besman-install-lf-java-roles.yml" "path=$HOME"
+    __besman_ansible_galaxy_install_from_requirements "$HOME/tmp" "$HOME/tmp"
+    __besman_ansible_playbook_extra_vars "besman-install-lf-java-roles.yml" "oah_command=install role_path=$HOME/tmp"
 
 }
