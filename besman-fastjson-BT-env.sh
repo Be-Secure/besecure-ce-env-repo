b0b2a4d93a8ffe0474fdde12ea78dcca6401f7d5
asa1997 <arunsureshampadath@gmail.com>
2022-07-18 13:22:42 +0530

    Updated env with todos.

--- besman-fastjson-BT-env.sh
+++ besman-fastjson-BT-env.sh
@@ -9,8 +9,6 @@ function __besman_install_fastjson-BT-env
     expoBT BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-bes-java:asa1997/ansible-role-bes-maven:asa1997/ansible-role-bes-eclipse
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
-    # return_val=$(__besman_check_for_trigger_playbook "$playbook")
-    # echo "Return val:$return_val"
     __besman_create_ansible_playbook "$playbook" "$BESMAN_ANSIBLE_GALAXY_ROLES"
 
     __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
@@ -27,21 +25,15 @@ function __besman_uninstall_fastjson-BT-env
     unset BESMAN_ANSIBLE_GALAXY_ROLES BESMAN_ANSIBLE_ROLE_PATH playbook 
 }
 
-function __besman_update_fastjson-BT-env
-{
-    local playbook
-    playbook=$HOME/besman-trigger-fastjson-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
-}
+# function __besman_update_fastjson-BT-env
+# {
+# # TODO
+# }
 
-function __besman_validate_fastjson-BT-env
-{
-    local playbook
-    playbook=$HOME/besman-trigger-fastjson-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
-}
+# function __besman_validate_fastjson-BT-env
+# {
+    #TODO
+# }
 
 # function __besman_reset_fastjson-BT-env
 # {
