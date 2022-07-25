697d831102da81a55a5ad1186c7b2861b1d93014
asa1997 <arunsureshampadath@gmail.com>
2022-07-25 16:52:06 +0530

    Role name updated

--- besman-fastjson-BT-env.sh
+++ besman-fastjson-BT-env.sh
@@ -9,7 +9,7 @@ function __besman_install_fastjson-BT-env
     __besman_gh_auth "$namespace"
     playbook=$HOME/besman-trigger-fastjson-BT-roles.yml
     export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
-    export BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-bes-java:asa1997/ansible-role-bes-maven:asa1997/ansible-role-bes-eclipse
+    export BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-oah-java:asa1997/ansible-role-oah-maven:asa1997/ansible-role-oah-eclipse
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
     __besman_check_for_trigger_playbook "$playbook"
@@ -27,7 +27,7 @@ function __besman_uninstall_fastjson-BT-env
     playbook=$HOME/besman-trigger-fastjson-BT-roles.yml
     __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
     [[ -f $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml ]] && rm $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml
-    rm -rf $BESMAN_ANSIBLE_ROLE_PATH/ansible-role-bes-*
+    rm -rf $BESMAN_ANSIBLE_ROLE_PATH/ansible-role-oah-*
     [[ -f $HOME/fastjson ]] && rm -rf $HOME/fastjson
     [[ -f $playbook ]] && rm $playbook
     unset BESMAN_ANSIBLE_GALAXY_ROLES BESMAN_ANSIBLE_ROLE_PATH playbook 
