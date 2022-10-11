7513b98f0d0c52005934a753ebf341fb4678f1ee
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2022-10-11 14:22:59 +0530

    Changed namespace

--- besman-fastjson-RT-env.sh
+++ besman-fastjson-RT-env.sh
@@ -10,7 +10,7 @@ function __besman_install_fastjson-RT-env
     __besman_gh_auth "$BESMAN_USER_NAMESPACE" || return 1
     playbook=$HOME/besman-trigger-fastjson-RT-roles.yml
     export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
-    export BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-oah-java:asa1997/ansible-role-oah-maven:asa1997/ansible-role-oah-eclipse:asa1997/ansible-role-oah-docker:asa1997/ansible-role-oah-sonarQube:asa1997/ansible-role-oah-sbomGenerator
+    export BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-oah-java:asa1997/ansible-role-oah-maven:asa1997/ansible-role-oah-eclipse:Pranshu021/ansible-role-oah-docker:Pranshu021/ansible-role-oah-sonarQube:Pranshu021/ansible-role-oah-sbomGenerator
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
     __besman_check_for_trigger_playbook "$playbook"
