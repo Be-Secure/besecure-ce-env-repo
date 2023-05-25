6fed09aa3696ecfdccde42841ade6aa887d6b8ed
pramit-d <pramit.dutta@wipro.com>
2023-05-25 23:07:16 +0530

    zaproxy BT env

---
+++ zaproxy/0.0.1/besman-zaproxy-BT-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function __besman_install_zaproxy-BT-env
+{
+    local playbook repo namespace clone_path
+    __besman_check_for_ansible || return 1
+    __besman_check_for_gh || return 1
+    namespace=Be-Secure
+    __besman_check_github_id || return 1
+    __besman_gh_auth "$BESMAN_USER_NAMESPACE"
+    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
+    export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
+    export BESMAN_ANSIBLE_GALAXY_ROLES=Be-Secure/ansible-role-oah-eclipse
+    __besman_update_requirements_file
+    __besman_ansible_galaxy_install_roles_from_requirements
+    __besman_check_for_trigger_playbook "$playbook"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook "$playbook" "$BESMAN_ANSIBLE_GALAXY_ROLES"
+    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
+    repo=zaproxy
+    clone_path=$HOME/$repo
+    [[ ! -d $clone_path ]] && __besman_gh_clone "$namespace" "$repo" "$clone_path"
+    unset playbook repo namespace clone_path
+}
