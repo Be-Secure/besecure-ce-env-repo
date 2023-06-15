908e6824862be0d28ea3d1da3568fc75b7c1035d
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-06-15 13:30:55 +0530

    Update besman-tomcat-BT-env.sh

--- tomcat/0.0.1/besman-tomcat-BT-env.sh
+++ tomcat/0.0.1/besman-tomcat-BT-env.sh
@@ -2,54 +2,53 @@
 
 function __besman_install_tomcat-BT-env
 {
-    local playbook repo namespace clone_path
-    __besman_check_for_ansible || return 1
+    
     __besman_check_for_gh || return 1
-    namespace=Be-Secure
     __besman_check_github_id || return 1
-    __besman_gh_auth "$BESMAN_USER_NAMESPACE"
-    playbook=$HOME/besman-trigger-tomcat-BT-roles.yml
-    export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
-    export BESMAN_ANSIBLE_GALAXY_ROLES=asa1997/ansible-role-oah-java:asa1997/ansible-role-oah-ant:asa1997/ansible-role-oah-eclipse
+    __besman_check_for_ansible || return 1
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
-    __besman_check_for_trigger_playbook "$playbook"
-    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook "$playbook" "$BESMAN_ANSIBLE_GALAXY_ROLES"
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    repo=tomcat
-    clone_path=$HOME/tomcat
-    [[ ! -d $clone_path ]] && __besman_gh_clone "$namespace" "$repo" "$clone_path"
-    unset playbook repo namespace clone_path
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "The clone path already contains dir names $BESMAN_OSSP"
+    else
+        __besman_gh_clone "$BESMAN_ORG" "$BESMAN_OSSP" "$BESMAN_OSSP_CLONE_PATH"
+    fi
+
 }
 
 function __besman_uninstall_tomcat-BT-env
 {
-    local playbook 
-    playbook=$HOME/besman-trigger-tomcat-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=remove role_path=$HOME/tmp" || return 1
-    [[ -f $HOME/tomcat ]] && rm -rf $HOME/tomcat
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "Removing $BESMAN_OSSP_CLONE_PATH..."
+        rm -rf "$BESMAN_OSSP_CLONE_PATH"
+    else
+        __besman_echo_yellow "Could not find dir $BESMAN_OSSP_CLONE_PATH"
+    fi
 }
 
 function __besman_update_tomcat-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-tomcat-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
 }
 
 function __besman_validate_tomcat-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-tomcat-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=validate role_path=$HOME/tmp" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
 }
 
 function __besman_reset_tomcat-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-tomcat-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
 }
