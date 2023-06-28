5f0e542b70c8df32537f094b248329b8b90c3269
pramit-d <pramit.dutta@wipro.com>
2023-06-28 16:50:00 +0530

    Added zaproxy RT BT env

--- zaproxy/0.0.1/besman-zaproxy-BT-env.sh
+++ zaproxy/0.0.1/besman-zaproxy-BT-env.sh
@@ -2,54 +2,62 @@
 
 function __besman_install_zaproxy-BT-env
 {
-    local playbook repo namespace clone_path
-    __besman_check_for_ansible || return 1
+    
     __besman_check_for_gh || return 1
-    namespace=Be-Secure
     __besman_check_github_id || return 1
-    __besman_gh_auth "$BESMAN_USER_NAMESPACE"
-    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
-    export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
-    export BESMAN_ANSIBLE_GALAXY_ROLES=pramit-d/ansible-role-bes-java:Be-Secure/ansible-role-oah-eclipse:Be-Secure/ansible-role-oah-gradle
+    __besman_check_for_ansible || return 1
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
-    __besman_check_for_trigger_playbook "$playbook"
-    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook "$playbook" "$BESMAN_ANSIBLE_GALAXY_ROLES"
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    repo=zaproxy
-    clone_path=$HOME/$repo
-    [[ ! -d $clone_path ]] && __besman_gh_clone "$namespace" "$repo" "$clone_path"
-    unset playbook repo namespace clone_path
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "The clone path already contains dir names $BESMAN_OSSP"
+    else
+        __besman_gh_clone "$BESMAN_ORG" "$BESMAN_OSSP" "$BESMAN_OSSP_CLONE_PATH"
+    fi
+    # Please add the rest of the code here for installation
+    bash
 }
 
 function __besman_uninstall_zaproxy-BT-env
 {
-    local playbook 
-    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=remove role_path=$HOME/tmp" || return 1
-    [[ -f $HOME/zaproxy ]] && rm -rf $HOME/zaproxy
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "Removing $BESMAN_OSSP_CLONE_PATH..."
+        rm -rf "$BESMAN_OSSP_CLONE_PATH"
+    else
+        __besman_echo_yellow "Could not find dir $BESMAN_OSSP_CLONE_PATH"
+    fi
+    # Please add the rest of the code here for uninstallation
+    bash
 }
 
 function __besman_update_zaproxy-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for update
+    bash
 }
 
 function __besman_validate_zaproxy-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=validate role_path=$HOME/tmp" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for validate
+    bash
 }
 
 function __besman_reset_zaproxy-BT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-BT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for reset
+    bash
 }
--- zaproxy/0.0.1/besman-zaproxy-RT-env.sh
+++ zaproxy/0.0.1/besman-zaproxy-RT-env.sh
@@ -2,60 +2,62 @@
 
 function __besman_install_zaproxy-RT-env
 {
-    local playbook repo namespace clone_path
-    __besman_check_for_ansible || return 1
+    
     __besman_check_for_gh || return 1
-    namespace=Be-Secure
     __besman_check_github_id || return 1
-    __besman_gh_auth "$BESMAN_USER_NAMESPACE" || return 1
-    playbook=$HOME/besman-trigger-zaproxy-RT-roles.yml
-    export BESMAN_ANSIBLE_ROLE_PATH=$HOME/tmp
-    export BESMAN_ANSIBLE_GALAXY_ROLES=pramit-d/ansible-role-bes-java:Be-Secure/ansible-role-oah-eclipse:Be-Secure/ansible-role-oah-gradle:Be-Secure/ansible-role-oah-docker:Be-Secure/ansible-role-oah-sonarQube:Be-Secure/ansible-role-oah-sbomGenerator:Be-Secure/ansible-role-oah-fossology
+    __besman_check_for_ansible || return 1
     __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements
-    __besman_check_for_trigger_playbook "$playbook"
-    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook "$playbook" "$BESMAN_ANSIBLE_GALAXY_ROLES"
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    repo=zaproxy
-    clone_path=$HOME/zaproxy
-    [[ ! -d $clone_path ]] && __besman_gh_clone "$namespace" "$repo" "$clone_path"
-    unset playbook repo namespace clone_path
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "The clone path already contains dir names $BESMAN_OSSP"
+    else
+        __besman_gh_clone "$BESMAN_ORG" "$BESMAN_OSSP" "$BESMAN_OSSP_CLONE_PATH"
+    fi
+    # Please add the rest of the code here for installation
+    bash
 }
 
 function __besman_uninstall_zaproxy-RT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-RT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    [[ -d $HOME/zaproxy ]] && rm -rf $HOME/zaproxy
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    if [[ -d $BESMAN_OSSP_CLONE_PATH ]]; then
+        __besman_echo_white "Removing $BESMAN_OSSP_CLONE_PATH..."
+        rm -rf "$BESMAN_OSSP_CLONE_PATH"
+    else
+        __besman_echo_yellow "Could not find dir $BESMAN_OSSP_CLONE_PATH"
+    fi
+    # Please add the rest of the code here for uninstallation
+    bash
 }
 
 function __besman_update_zaproxy-RT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-RT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for update
+    bash
 }
 
 function __besman_validate_zaproxy-RT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-RT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-   if [[ -d $HOME/zaproxy ]]; then
-        __besman_echo_green "zaproxy found"
-   else
-        __besman_echo_red "zaproxy not found"
-   fi
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for validate
+    bash
 }
 
 function __besman_reset_zaproxy-RT-env
 {
-    local playbook
-    playbook=$HOME/besman-trigger-zaproxy-RT-roles.yml
-    __besman_run_ansible_playbook_extra_vars "$playbook" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLE_PATH" || return 1
-    unset playbook
+    __besman_check_for_trigger_playbook "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLES_PATH"
+    # Please add the rest of the code here for reset
+    bash
 }
-
