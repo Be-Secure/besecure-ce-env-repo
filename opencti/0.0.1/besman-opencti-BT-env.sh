2d6c12a81fe96a90e3aaa8a2883cc4d0e1b74cff
NeerajK007 <neeraj.kumar32@wipro.com>
2024-02-22 09:07:54 +0530

    BR-RT environment for opencti project & added packagemanager and IDE for newrelic-java-agent

--- opencti/0.0.1/besman-opencti-BT-env.sh
+++ opencti/0.0.1/besman-opencti-BT-env.sh
@@ -18,20 +18,12 @@ function __besman_install_opencti-BT-env {
 
     # Please add the rest of the code here for installation
     # opencti-dev environment script
-    echo -e "\nopencti dev modules setup start..."
+    echo "1 ------- opencti dev modules setup..."
     #!/bin/bash
-
-    #------------------------------------------------------------------------------------------
-    # Check if Docker is installed
-    if ! command -v docker &>/dev/null; then
-        install_docker
-    else
-        echo -e "\nDocker is already there to use."
-    fi
 
     # Function to install Docker
     install_docker() {
-        echo -e "\nInstalling Docker..."
+        echo "Installing Docker..."
         # Update package index
         sudo apt update
         # Install dependencies
@@ -47,10 +39,26 @@ function __besman_install_opencti-BT-env {
         sudo apt install -y docker-ce docker-ce-cli containerd.io
         # Add current user to Docker group
         sudo usermod -aG docker $USER
-        echo -e "\nDocker installed successfully.\n"
+        echo "Docker installed successfully."
     }
 
-    #------------------------------------------------------------------------------------------
+    # Check if Docker is installed
+    if ! command -v docker &>/dev/null; then
+        install_docker
+    else
+        echo "Docker is already there to use."
+    fi
+
+    # Function to stop and remove a container if it's running
+    stop_and_remove_container() {
+        local container_name=$1
+        if docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
+            echo "Stopping and removing existing $container_name container..."
+            docker stop "$container_name" &>/dev/null
+            docker rm "$container_name" &>/dev/null
+        fi
+    }
+
     # Stop and remove existing containers if they are already running
     stop_and_remove_container "opencti-dev-redis"
     stop_and_remove_container "opencti-dev-redis-insight"
@@ -60,36 +68,17 @@ function __besman_install_opencti-BT-env {
     stop_and_remove_container "opencti-dev-rabbitmq"
     stop_and_remove_container "opencti-dev-jaegertracing"
 
-    # Function to stop and remove a container if it's running
-    stop_and_remove_container() {
-        local container_name=$1
-        if docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
-            echo -e "\nStopping and removing existing $container_name container..."
-            docker stop "$container_name" &>/dev/null
-            docker rm "$container_name" &>/dev/null
-        fi
-    }
-
-    #------------------------------------------------------------------------------------------
-    # Now pull the images and start the containers
-    # Define variables to store image tags
-    global REDIS_IMAGE="redis:7.2.4"
-    global REDIS_INSIGHT_IMAGE="redislabs/redisinsight:latest"
-    global ELASTICSEARCH_IMAGE="docker.elastic.co/elasticsearch/elasticsearch:8.12.0"
-    global KIBANA_IMAGE="docker.elastic.co/kibana/kibana:8.12.0"
-    global MINIO_IMAGE="minio/minio:latest"
-    global RABBITMQ_IMAGE="rabbitmq:3.12-management"
-    global JAEGERT_IMAGE="jaegertracing/all-in-one:latest"
+    # Now start the containers
     # Starting Redis
-    echo -e "\nStarting Redis..."
-    docker run -d --name opencti-dev-redis -p 6379:6379 $REDIS_IMAGE
+    echo "Starting Redis..."
+    docker run -d --name opencti-dev-redis -p 6379:6379 redis:7.2.4
 
     # Starting Redis Insight
-    echo -e "\nStarting Redis Insight..."
-    docker run -d --name opencti-dev-redis-insight -p 8001:8001 $REDIS_INSIGHT_IMAGE
+    echo "Starting Redis Insight..."
+    docker run -d --name opencti-dev-redis-insight -p 8001:8001 redislabs/redisinsight:latest
 
     # Starting Elasticsearch
-    echo -e "\nStarting Elasticsearch..."
+    echo "Starting Elasticsearch..."
     docker run -d --name opencti-dev-elasticsearch \
         -p 9200:9200 -p 9300:9300 \
         -v esdata:/usr/share/elasticsearch/data \
@@ -99,47 +88,47 @@ function __besman_install_opencti-BT-env {
         -e "xpack.security.enabled=false" \
         -e "ES_JAVA_OPTS=-Xms2G -Xmx2G" \
         --ulimit memlock=-1:-1 --ulimit nofile=65536:65536 \
-        $ELASTICSEARCH_IMAGE
+        docker.elastic.co/elasticsearch/elasticsearch:8.12.0
 
     # Starting Kibana
-    echo -e "\nStarting Kibana..."
+    echo "Starting Kibana..."
     docker run -d --name opencti-dev-kibana \
         -p 5601:5601 \
         -e "ELASTICSEARCH_HOSTS=http://localhost:9200" \
         --link opencti-dev-elasticsearch:elasticsearch \
-        $KIBANA_IMAGE
+        docker.elastic.co/kibana/kibana:8.12.0
 
     # Starting Minio
-    echo -e "\nStarting Minio..."
+    echo "Starting Minio..."
     docker run -d --name opencti-dev-minio \
         -p 9000:9000 -p 9001:9001 -p 35300:35300 \
         -e "MINIO_ROOT_USER=ChangeMe" \
         -e "MINIO_ROOT_PASSWORD=ChangeMe" \
-        $MINIO_IMAGE server /data --console-address ":9001"
+        minio/minio:latest server /data --console-address ":9001"
 
     # Starting RabbitMQ
-    echo -e "\nStarting RabbitMQ..."
+    echo "Starting RabbitMQ..."
     docker run -d --name opencti-dev-rabbitmq \
         -p 5672:5672 -p 15672:15672 \
-        $RABBITMQ_IMAGE
+        rabbitmq:3.12-management
 
     # Starting Jaeger Tracing
-    echo -e "\nStarting Jaeger Tracing..."
+    echo "Starting Jaeger Tracing..."
     docker run -d --name opencti-dev-jaegertracing \
         -p 16686:16686 -p 4318:4318 \
-        $JAEGERT_IMAGE
+        jaegertracing/all-in-one:latest
 
-    echo -e "\nAll services started successfully!"
+    echo "All services started successfully!"
 
-    echo -e "\ndev-module setup completed."
+    echo "1 ------- dev-module setup completed."
+    print "/n"
 
-    #------------------------------------------------------------------------------------------
-    echo -e "\ngraphql setup start...."
+    echo "2 ------- graphql setup...."
     #!/bin/bash
 
     # Check if Yarn is installed
     if ! command -v yarn &>/dev/null; then
-        echo -e "\nYarn is not installed. Installing Yarn..."
+        echo "Yarn is not installed. Installing Yarn..."
         # Add Yarn repository key
         curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
         # Add Yarn repository
@@ -161,10 +150,10 @@ function __besman_install_opencti-BT-env {
     # Start GraphQL server
     yarn start &
 
-    echo -e "graphql setup completed....\n"
+    echo "2 ------- graphql setup completed...."
+    print "/n"
 
-    #------------------------------------------------------------------------------------------
-    echo -e "\nworker setup start...."
+    echo "3 ------- worker setup...."
     #!/bin/bash
 
     # Check if Python is installed
@@ -190,10 +179,10 @@ function __besman_install_opencti-BT-env {
     # Start the worker
     python3 worker.py &
 
-    echo -e "worker setup completed....\n"
+    echo "3 ------- worker setup completed...."
+    print "/n"
 
-    #------------------------------------------------------------------------------------------
-    echo -e "\nfront-end setup start....\n"
+    echo "4 ------- front-end setup...."
     #!/bin/bash
 
     # Check if Yarn is installed
@@ -225,11 +214,9 @@ function __besman_install_opencti-BT-env {
     sleep 30 # Wait for the server to start (you may adjust the duration if needed)
     firefox http://localhost:4000
 
-    echo -e "\nfront-end setup completed...."
-
-    echo -e "\nopencti local setup completed.\n"
-
-    #------------------------------------------------------------------------------------------
+    echo "OpenCTI Frontend is now running!"
+    echo "4 ------- front-end setup completed...."
+    print "/n"
 }
 
 function __besman_uninstall_opencti-BT-env {
@@ -244,47 +231,6 @@ function __besman_uninstall_opencti-BT-env {
     fi
     # Please add the rest of the code here for uninstallation
 
-    # Stop and remove existing containers if they are already running
-    stop_and_remove_container "opencti-dev-redis"
-    stop_and_remove_container "opencti-dev-redis-insight"
-    stop_and_remove_container "opencti-dev-elasticsearch"
-    stop_and_remove_container "opencti-dev-kibana"
-    stop_and_remove_container "opencti-dev-minio"
-    stop_and_remove_container "opencti-dev-rabbitmq"
-    stop_and_remove_container "opencti-dev-jaegertracing"
-
-    # Function to stop and remove a container if it's running
-    stop_and_remove_container() {
-        local container_name=$1
-        if docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
-            echo -e "\nStopping and removing existing $container_name container..."
-            docker stop "$container_name" &>/dev/null
-            docker rm "$container_name" &>/dev/null
-        fi
-    }
-
-    # Remove Docker images
-    docker rmi $REDIS_IMAGE $REDIS_INSIGHT_IMAGE $ELASTICSEARCH_IMAGE $KIBANA_IMAGE $MINIO_IMAGE $RABBITMQ_IMAGE $JAEGERT_IMAGE
-
-    # Remove Docker package
-    sudo apt purge docker-ce docker-ce-cli containerd.io
-
-    # Remove user from Docker group (if needed)
-    sudo deluser $USER docker
-
-    # Remove packages
-    sudo apt remove docker-ce docker-ce-cli containerd.io
-
-    # Uninstall Yarn
-    sudo apt purge yarn
-
-    # Remove Yarn repository file
-    sudo rm /etc/apt/sources.list.d/yarn.list
-
-    # Update package index
-    sudo apt update
-
-    echo -e "\nUninstallation completed successfully.\n"
 }
 
 function __besman_update_opencti-BT-env {
@@ -300,93 +246,6 @@ function __besman_validate_opencti-BT-env {
     [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
     __besman_run_ansible_playbook_extra_vars "$BESMAN_OSS_TRIGGER_PLAYBOOK_PATH/$BESMAN_OSS_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
     # Please add the rest of the code here for validate
-
-    # Check if Docker is installed
-    if ! command -v docker &>/dev/null; then
-        echo "Error: Docker is not installed."
-        exit 1
-    else
-        echo -e "Docker is installed"
-    fi
-
-    # Function to check if a Docker image exists
-    check_docker_image() {
-        local image_name=$1
-        if ! docker image inspect "$image_name" &>/dev/null; then
-            echo "Error: Docker image $image_name not found."
-            exit 1
-        else
-            echo -e "Docker image $image_name is installed"
-        fi
-    }
-
-    # Function to check if a Docker container exists
-    check_docker_container() {
-        local container_name=$1
-        if ! docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
-            echo "Error: Docker container $container_name not found."
-            exit 1
-        else
-            echo -e "Docker container $container_name is available"
-        fi
-    }
-
-    # Check if Docker images exist
-    check_docker_image "$REDIS_IMAGE"
-    check_docker_image "$REDIS_INSIGHT_IMAGE"
-    check_docker_image "$ELASTICSEARCH_IMAGE"
-    check_docker_image "$KIBANA_IMAGE"
-    check_docker_image "$MINIO_IMAGE"
-    check_docker_image "$RABBITMQ_IMAGE"
-    check_docker_image "$JAEGERT_IMAGE"
-
-    # Check if Docker containers exist
-    check_docker_container "opencti-dev-redis"
-    check_docker_container "opencti-dev-redis-insight"
-    check_docker_container "opencti-dev-elasticsearch"
-    check_docker_container "opencti-dev-kibana"
-    check_docker_container "opencti-dev-minio"
-    check_docker_container "opencti-dev-rabbitmq"
-    check_docker_container "opencti-dev-jaegertracing"
-
-    echo "All Docker images and containers are available."
-
-    # Function to check if Python 3 is installed
-    check_python3() {
-        if ! command -v python3 &>/dev/null; then
-            echo "Error: Python 3 is not installed."
-            exit 1
-        else
-            echo -e "Python3 is installed"
-        fi
-    }
-
-    # Function to check if Pip is installed
-    check_pip() {
-        if ! command -v pip &>/dev/null; then
-            echo "Error: Pip is not installed."
-            exit 1
-        else
-            echo -e "Pip is installed"
-        fi
-    }
-
-    # Function to check if Yarn is installed
-    check_yarn() {
-        if ! command -v yarn &>/dev/null; then
-            echo "Error: Yarn is not installed."
-            exit 1
-        else
-            echo -e "Yarn is installed"
-        fi
-    }
-
-    # Call functions to check for Yarn, Python 3, and Pip
-    check_python3
-    check_pip
-    check_yarn
-
-    echo "All required tools are available."
 
 }
 
