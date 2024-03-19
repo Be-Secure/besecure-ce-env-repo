6aeeba5212670d002922a852ee4b0e0532ce1d15
asa1997 <arunsureshampadath@gmail.com>
2024-03-20 00:05:16 +0530

    Added fastjson rt env 0.0.2

---
+++ fastjson/0.0.2/besman-fastjson-RT-env.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+function __besman_install_fastjson-RT-env
+{
+    __besman_check_for_gh || return 1 # Checks if GitHub CLI is present or not.
+    __besman_check_github_id || return 1 # checks whether the user github id has been populated or not under BESMAN_USER_NAMESPACE 
+    __besman_check_for_ansible || return 1 # Checks if ansible is installed or not.
+    
+    # Requirements file is used to list the required ansible roles. The data for requirements file comes from BESMAN_ANSIBLE_ROLES env var.
+    # This function updates the requirements file from BESMAN_ANSIBLE_ROLES env var.
+    __besman_update_requirements_file 
+    __besman_ansible_galaxy_install_roles_from_requirements # Downloads the ansible roles mentioned in BESMAN_ANSIBLE_ROLES to BESMAN_ANSIBLE_ROLES_PATH
+    # This function checks for the playbook BESMAN_ARTIFACT_TRIGGER_PLAYBOOK under BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH.
+    # The trigger playbook is used to run the ansible roles.
+    __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook # Creates the trigger playbook if not present.
+    # Runs the trigger playbook. We are also passing these variables - bes_command=install; role_path=$BESMAN_ANSIBLE_ROLES_PATH
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=install role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    # Clones the source code repo.
+    if [[ -d $BESMAN_ARTIFACT_DIR ]]; then
+        __besman_echo_white "The clone path already contains dir names $BESMAN_ARTIFACT_NAME"
+    else
+        __besman_gh_clone "$BESMAN_ORG" "$BESMAN_ARTIFACT_NAME" "$BESMAN_ARTIFACT_DIR"
+        git checkout -b "$BESMAN_ARTIFACT_VERSION"_tavoss 1.2.24
+    fi
+
+    if [[ -d $BESMAN_ASSESSMENT_DATASTORE_DIR ]] 
+    then
+        __besman_echo_white "Assessment datastore found at $BESMAN_ASSESSMENT_DATASTORE_DIR"
+    else
+        __besman_gh_clone "$BESMAN_ORG" "besecure-assessment-datastore" "$BESMAN_ASSESSMENT_DATASTORE_DIR"
+
+    fi
+    # Please add the rest of the code here for installation
+}
+
+function __besman_uninstall_fastjson-RT-env
+{
+    __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    if [[ -d $BESMAN_ARTIFACT_DIR ]]; then
+        __besman_echo_white "Removing $BESMAN_ARTIFACT_DIR..."
+        rm -rf "$BESMAN_ARTIFACT_DIR"
+    else
+        __besman_echo_yellow "Could not find dir $BESMAN_ARTIFACT_DIR"
+    fi
+    # Please add the rest of the code here for uninstallation
+
+}
+
+function __besman_update_fastjson-RT-env
+{
+    __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    # Please add the rest of the code here for update
+
+}
+
+function __besman_validate_fastjson-RT-env
+{
+    __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    # Please add the rest of the code here for validate
+
+}
+
+function __besman_reset_fastjson-RT-env
+{
+    __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
+    [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
+    __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
+    # Please add the rest of the code here for reset
+
+}
