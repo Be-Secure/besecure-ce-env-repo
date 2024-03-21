d06161dc11a0f11f9be880bafdcd0d94dca14c82
asa1997 <arun.suresh@wipro.com>
2024-03-21 10:49:56 +0530

    Added test

---
+++ fastjson/0.0.3/besman-fastjson-BT-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function __besman_install_fastjson-BT-env
+{
+    __besman_echo_white "Installed"
+}
+
+function __besman_uninstall_fastjson-BT-env
+{
+    __besman_echo_white "Uninstalled"
+}
