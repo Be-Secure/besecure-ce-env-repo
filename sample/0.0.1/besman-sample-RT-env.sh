cce4e5eee732c0cecf3025e11490f41434cbf1f9
asa1997 <arun.suresh@wipro.com>
2024-07-04 09:34:53 +0530

    Updated

---
+++ sample/0.0.1/besman-sample-RT-env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function __besman_install
+{
+    echo  "Installing"
+}
+
+# function __besman_uninstall
+# {
+    
+# }
+
+# function __besman_update
+# {
+    
+# }
+
+# function __besman_validate
+# {
+    
+# }
+
+# function __besman_reset
+# {
+    
+# }
