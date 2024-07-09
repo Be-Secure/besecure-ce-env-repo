1ba91b9ad995782c4e3317d38dcdd54d4b9bd9ec
root <root@ip-172-31-5-90.ap-south-1.compute.internal>
2024-07-09 13:00:36 +0000

    fix code for env

--- LlmAssessment/0.0.1/besman-LlmAssessment-RT-env.sh
+++ LlmAssessment/0.0.1/besman-LlmAssessment-RT-env.sh
@@ -15,7 +15,10 @@ function __besman_install
 
 function __besman_uninstall
 {
-    
+   echo "------------------------------------------------------"
+   echo "uninstall PurpleLama..."
+   echo "------------------------------------------------------"
+   uninstall_PurpleLlama
 }
 
 function __besman_update
@@ -34,43 +37,50 @@ function __besman_reset
 }
 
 
-function install_purpleLlama() {
+function install_purpleLlama() 
+{
     echo "Cloning the PurpleLama repository..."
     git clone --single-branch --branch $BESMAN_PURPLELAMA_BRANCH $BESMAN_PURPLELAMA_URL $BESMAN_PURPLELAMA_LOCAL_PATH
-
+    if [["$?" != 0]]; then
+        echo "error while cloning"
+	return 1
+    fi
     echo "Installing required dependency..."
     sudo apt -y update
     sudo apt -y upgrade
     sudo apt install -y python3-pip
+    apt  install -y cargo
+    apt install python3.10-venv
     cargo install weggli --rev=9d97d462854a9b682874b259f70cc5a97a70f2cc --git=https://github.com/weggli-rs/weggli
     export WEGGLI_PATH=weggli
     python3 -m venv ~/.venvs/CybersecurityBenchmarks
     source ~/.venvs/CybersecurityBenchmarks/bin/activate
     export DATASETS=$PWD/CybersecurityBenchmarks/datasets
-
+    if [["$?" != 0]]; then
+        echo "error while installing dependency"
+        return 1
+    fi
     echo "Installing Python packages from requirements.txt..."
     cd $BESMAN_PURPLELAMA_LOCAL_PATH
     pip3 install -r CybersecurityBenchmarks/requirements.txt
     python3 -m CybersecurityBenchmarks.benchmark.run --help
+    if [["$?" != 0]]; then
+        echo "error while installing packages"
+        return 1
+    fi
 }
 
-function uninstall_PurpleLlama(){
-    AUTO_DELETE=${1:-false}
-    
-    if [ "$AUTO_DELETE" = true ]; then
-        echo "Removing the directory '$BESMAN_PURPLELAMA_LOCAL_PATH'..."
-        rm -rf $BESMAN_PURPLELAMA_LOCAL_PATH && echo "Directory 'PurpleLlama' has been removed." || echo "Failed to remove the directory 'PurpleLlama'."
-    else
-        read -p "${bold}Do you want to remove the directory '$BESMAN_PURPLELAMA_LOCAL_PATH'? (y/n): " response
-        if [[ "$response" == "y" || "$response" == "Y" || "$response" == "Yes" || "$response" == "yes" ]]; then
-            if rm -rf $BESMAN_PURPLELAMA_LOCAL_PATH; then
-                echo "Directory '$BESMAN_PURPLELAMA_LOCAL_PATH' has been removed."
-            else
-                echo "Failed to remove the directory '$BESMAN_PURPLELAMA_LOCAL_PATH'."
-            fi
+function uninstall_PurpleLlama()
+{
+    read -p "${bold}Do you want to remove the directory '$BESMAN_PURPLELAMA_LOCAL_PATH'? (y/n): " response
+    if [[ "$response" == "y" || "$response" == "Y" || "$response" == "Yes" || "$response" == "yes" ]]; then
+        if rm -rf $BESMAN_PURPLELAMA_LOCAL_PATH; then
+            echo "Directory '$BESMAN_PURPLELAMA_LOCAL_PATH' has been removed."
         else
-            echo "Skipping the removal of '$BESMAN_PURPLELAMA_LOCAL_PATH' directory..."
+            echo "Failed to remove the directory '$BESMAN_PURPLELAMA_LOCAL_PATH'."
         fi
+    else
+        echo "Skipping the removal of '$BESMAN_PURPLELAMA_LOCAL_PATH' directory..."
     fi
 }
 
