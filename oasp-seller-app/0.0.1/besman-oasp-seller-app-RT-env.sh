9372d13139130b4e18d8e87b9ebf0b25d7c598ab
NeerajK007 <neeraj.kumar32@wipro.com>
2024-10-24 14:38:32 +0530

    oasp-seller-app RT env update

--- oasp-seller-app/0.0.1/besman-oasp-seller-app-RT-env.sh
+++ oasp-seller-app/0.0.1/besman-oasp-seller-app-RT-env.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-function __besman_install
-{
+function __besman_install {
 
-    __besman_check_vcs_exist || return 1 # Checks if GitHub CLI is present or not.
-    __besman_check_github_id || return 1 # checks whether the user github id has been populated or not under BESMAN_USER_NAMESPACE 
+    __besman_check_vcs_exist || return 1   # Checks if GitHub CLI is present or not.
+    __besman_check_github_id || return 1   # checks whether the user github id has been populated or not under BESMAN_USER_NAMESPACE
     __besman_check_for_ansible || return 1 # Checks if ansible is installed or not.
-    __besman_create_roles_config_file # Creates the role config file with the parameters from env config
-    
+    __besman_create_roles_config_file      # Creates the role config file with the parameters from env config
+
     # Requirements file is used to list the required ansible roles. The data for requirements file comes from BESMAN_ANSIBLE_ROLES env var.
     # This function updates the requirements file from BESMAN_ANSIBLE_ROLES env var.
-    __besman_update_requirements_file 
+    __besman_update_requirements_file
     __besman_ansible_galaxy_install_roles_from_requirements # Downloads the ansible roles mentioned in BESMAN_ANSIBLE_ROLES to BESMAN_ANSIBLE_ROLES_PATH
     # This function checks for the playbook BESMAN_ARTIFACT_TRIGGER_PLAYBOOK under BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH.
     # The trigger playbook is used to run the ansible roles.
@@ -28,8 +27,7 @@ function __besman_install
         cd "$HOME"
     fi
 
-    if [[ -d $BESMAN_ASSESSMENT_DATASTORE_DIR ]] 
-    then
+    if [[ -d $BESMAN_ASSESSMENT_DATASTORE_DIR ]]; then
         __besman_echo_white "Assessment datastore found at $BESMAN_ASSESSMENT_DATASTORE_DIR"
     else
         __besman_echo_white "Cloning assessment datastore from $\BESMAN_USER_NAMESPACE/besecure-assessment-datastore"
@@ -37,10 +35,23 @@ function __besman_install
 
     fi
     # Please add the rest of the code here for installation
+
+    install_CommonDep
+
+    [[ ! -z $BESMAN_ASSESSMENT_TOOLS ]] && readarray -d ',' -t ASSESSMENT_TOOLS <<<"$BESMAN_ASSESSMENT_TOOLS"
+
+    if [ ! -z $ASSESSMENT_TOOLS ]; then
+        for tool in ${ASSESSMENT_TOOLS[*]}; do
+
+            install_$tool
+
+        done
+        echo "bes assessment tools installation done"
+    fi
+
 }
 
-function __besman_uninstall
-{
+function __besman_uninstall {
     __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
     [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
     __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=remove role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
@@ -54,8 +65,7 @@ function __besman_uninstall
 
 }
 
-function __besman_update
-{
+function __besman_update {
     __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
     [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
     __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=update role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
@@ -63,8 +73,7 @@ function __besman_update
 
 }
 
-function __besman_validate
-{
+function __besman_validate {
     __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
     [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
     __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=validate role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
@@ -72,11 +81,61 @@ function __besman_validate
 
 }
 
-function __besman_reset
-{
+function __besman_reset {
     __besman_check_for_trigger_playbook "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK"
     [[ "$?" -eq 1 ]] && __besman_create_ansible_playbook
     __besman_run_ansible_playbook_extra_vars "$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK_PATH/$BESMAN_ARTIFACT_TRIGGER_PLAYBOOK" "bes_command=reset role_path=$BESMAN_ANSIBLE_ROLES_PATH" || return 1
     # Please add the rest of the code here for reset
+
+}
+
+###
+## function for each bes tool declared in steps file
+###
+
+function install_CommonDep {
+
+    # Check if Docker is installed
+    if ! command -v docker &>/dev/null; then
+        echo "Docker is not installed. Installing Docker..."
+
+        # Docker installation steps
+        sudo apt update
+        sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+        sudo apt update
+        sudo apt-cache policy docker-ce
+        sudo apt install -y docker-ce
+        sudo systemctl status docker --no-pager
+
+        echo "Docker installation completed."
+    else
+        echo "Docker is already installed."
+    fi
+
+    return 0
+}
+
+function install_sonarqube {
+
+    return 0
+}
+
+function install_scorecard {
+
+    return 0
+
+}
+
+function install_criticality_score {
+
+    return 0
+
+}
+
+function install_spdx-sbom-generator {
+
+    return 0
 
 }
