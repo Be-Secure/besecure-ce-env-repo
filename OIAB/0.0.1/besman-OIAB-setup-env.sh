513e17acbec043b6208a55fe08efc70071e2509c
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2024-11-30 19:15:39 +0530

    updated

--- OIAB/0.0.1/besman-OIAB-setup-env.sh
+++ OIAB/0.0.1/besman-OIAB-setup-env.sh
@@ -162,7 +162,7 @@ function __besman_install_ossverse_network() {
 function __besman_copy_layer2_config() {
 
     local layer2_config_dir="$BESMAN_BECKN_ONIX_DIR/layer2/samples"
-    local layer2_file="Software Assurance_1.0.0.yaml"
+    local layer2_file="Software\ Assurance_1.0.0.yaml"
     local source_path="$layer2_config_dir/$layer2_file"
     local destination_path="/usr/src/app/schemas"
     __besman_echo_white "Copying layer2 config files"
@@ -191,7 +191,7 @@ function __besman_copy_layer2_config() {
         __besman_echo_red "Failed to copy config to bpp-client"
         return 1
     }
-    
+    sleep 10
     __besman_echo_white "Restarting containers"
     docker restart bap-client bap-network bpp-network bpp-client || {
         __besman_echo_red "Failed to restart containers"
