515530534317c3690c0d0c52838f560d2fb44b6e
asa1997 <arunsureshampadath@gmail.com>
2024-12-02 18:12:21 +0530

    added steps to setup seller app

--- OIAB/0.0.1/besman-OIAB-setup-env.sh
+++ OIAB/0.0.1/besman-OIAB-setup-env.sh
@@ -72,7 +72,7 @@ function __besman_install_buyer_ui() {
         __besman_echo_white "Updated nginx.conf port to 8001"
     fi
 
-    docker build -t oiab-buyer-ui .
+    docker build --build-arg VITE_API_BASE_URL="$BESMAN_IP_ADDRESS:8000" -t oiab-buyer-ui .
     docker run -d --name oiab-buyer-ui -p 8001:8001 oiab-buyer-ui
     cd "$HOME" || return 1
 
@@ -96,7 +96,7 @@ function __besman_install_buyer_app() {
         __besman_echo_white "Updated MongoDB port mapping to 27018:27017"
     fi
 
-    sed -i "s|PROTOCOL_SERVER_URL=.*|PROTOCOL_SERVER_URL=$BESMAN_PROTOCOL_SERVER_URL|g" "$env_file"
+    sed -i "s|PROTOCOL_SERVER_URL=.*|PROTOCOL_SERVER_URL=$BESMAN_IP_ADDRESS:5001|g" "$env_file"
     sed -i "s|BAP_ID=.*|BAP_ID=$BESMAN_BAP_ID|g" "$env_file"
     sed -i "s|BAP_URI=.*|BAP_URI=$BESMAN_BAP_URI|g" "$env_file"
 
@@ -216,6 +216,198 @@ function __besman_copy_layer2_config() {
     __besman_echo_green "Copied layer2 config files"
     return 0
 }
+function __besman_update_cors_and_config() {
+    
+    __besman_echo_white "Updating CORS and config"
+
+    local seller_cors_file="$BESMAN_SELLER_APP_DIR/seller/app/config/environments/base/env.cors.js"
+    local seller_api_config="$BESMAN_SELLER_APP_DIR/seller-app-api/lib/config/production_env_config.json"
+
+    [[ -f $seller_cors_file ]] && rm "$seller_cors_file"
+    touch "$seller_cors_file"
+
+    cat <<EOF >"$seller_cors_file"
+module.exports = {
+    'cors': {
+        'whitelistUrls': [
+            '*',
+            '$BESMAN_IP_ADDRESS'
+        ]
+    }
+};
+EOF
+
+    [[ -f $seller_api_config ]] && rm "$seller_api_config"
+    touch "$seller_api_config"
+    cat <<EOF >"$seller_api_config"
+{
+  "express": {
+    "protocol": "http://",
+    "useFqdnForApis": false,
+    "ipAddress": "strapi",
+    "fqdn": "",
+    "port": 3001,
+    "apiUrl": ""
+  },
+  "auth": {
+    "token": {
+      "access": {
+        "exp": 8640000,
+        "secret": "wftd3hg5$g67h*fd5h6fbvcy6rtg5wftd3hg5$g67h*fd5xxx"
+      },
+      "resetPasswordLink": {
+        "exp": 86400,
+        "secret": "de$rfdf5$g67*jhu*sdfbvcy3frd6r4e"
+      }
+    }
+  },
+  "database": {
+    "username": "strapi",
+    "password": "strapi",
+    "name": "sellerapp",
+    "host": "postgres",
+    "port": "5432",
+    "dialect": "postgres",
+    "pool": {
+      "max": 60,
+      "min": 0,
+      "acquire": 1000000,
+      "idle": 10000,
+      "evict": 10000
+    }
+  },
+  "email": {
+    "transport": {
+      "SMTP": {
+        "host": "",
+        "port": 587,
+        "secure": false,
+        "auth": {
+          "user": "",
+          "pass": ""
+        }
+      },
+      "local": {
+        "sendmail": true,
+        "newline": "unix",
+        "path": "/usr/sbin/sendmail"
+      }
+    },
+    "sender": "",
+    "supportEmail": "",
+    "webClientUri": "http://34.123.120.224",
+    "shareUrl": "https://www.google.com"
+  },
+  "cors": {
+    "whitelistUrls": [
+      "http://strapi:3001",
+      "$BESMAN_IP_ADDRESS"
+    ]
+  },
+  "directory": {
+    "profilePictures": "PROFILE_PICTURES"
+  },
+  "cookieOptions": {
+    "httpOnly": true,
+    "secure": false,
+    "sameSite": false
+  },
+  "general": {
+    "exceptionEmailRecipientList": []
+  },
+  "seller": {
+    "serverUrl": "http://seller:3008"
+  },
+  "firebase": {
+    "account": ""
+  },
+  "sellerConfig": {
+    "BPP_ID": "sellerapp-staging.datasyndicate.in",
+    "BPP_URI": "$BESMAN_IP_ADDRESS:6001",
+    "BAP_ID": "sellerapp-staging.datasyndicate.in",
+    "BAP_URI": "https://7e3b-2401-4900-1c5d-2b13-814b-de08-9df3-b44e.ngrok-free.app",
+    "storeOpenSchedule": {
+      "time": {
+        "days": "1,2,3,4,5,6,7",
+        "schedule": {
+          "holidays": [
+            "2022-08-15",
+            "2022-08-19"
+          ],
+          "frequency": "PT4H",
+          "times": [
+            "1100",
+            "1900"
+          ]
+        },
+        "range": {
+          "start": "1100",
+          "end": "2100"
+        }
+      }
+    },
+    "sellerPickupLocation": {
+      "person": {
+        "name": "Ramu"
+      },
+      "location": {
+        "gps": "12.938382, 77.651775",
+        "address": {
+          "area_code": "560087",
+          "name": "Fritoburger",
+          "building": "12 Restaurant Tower",
+          "locality": "JP Nagar 24th Main",
+          "city": "Bengaluru",
+          "state": "Karnataka",
+          "country": "India"
+        }
+      },
+      "contact": {
+        "phone": "98860 98860",
+        "email": "abcd.efgh@gmail.com"
+      }
+    }
+  },
+  "settlement_details": [
+    {
+      "settlement_counterparty": "buyer-app",
+      "settlement_type": "upi",
+      "upi_address": "gft@oksbi",
+      "settlement_bank_account_no": "XXXXXXXXXX",
+      "settlement_ifsc_code": "XXXXXXXXX"
+    }
+  ]
+}
+EOF
+
+    # If any of the above files are not present, return an error
+    if [[ ! -f $seller_cors_file ]] || [[ ! -f $seller_api_config ]]; then
+        __besman_echo_red "Failed to update CORS and config"
+        return 1
+    fi
+
+}
+
+function __besman_install_seller_app() {
+
+    __besman_echo_yellow "Installing seller app"
+    if [[ -d $BESMAN_SELLER_APP_DIR ]] 
+    then
+        __besman_echo_no_colour "Found seller app source code"
+
+    else
+        __besman_echo_white "Cloning seller app source code"
+        __besman_repo_clone "$BESMAN_ORG" "$BESMAN_SELLER_APP_SOURCE_REPO" "$BESMAN_SELLER_APP_DIR" || return 1
+    fi
+
+    cd "$BESMAN_SELLER_APP_DIR" || return 1
+
+    __besman_update_cors_and_config || return 1
+
+    __besman_echo_white "Building seller app"
+    docker-compose up --build -d
+
+}
 
 
 function __besman_install {
@@ -230,6 +422,7 @@ function __besman_install {
     __besman_install_beckn_onix || return 1
     __besman_install_ossverse_network || return 1
     # __besman_copy_layer2_config || return 1
+    __besman_install_seller_app || return 1
 }
 
 function __besman_uninstall {
@@ -251,6 +444,18 @@ function __besman_uninstall {
     if [[ -d $BESMAN_OIAB_BUYER_APP_DIR ]]; then
         rm -rf "$BESMAN_OIAB_BUYER_APP_DIR"
         __besman_echo_yellow "Removed buyer app directory"
+    fi
+
+    # remove beckn-onix dir
+    if [[ -d $BESMAN_BECKN_ONIX_DIR ]]; then
+        rm -rf "$BESMAN_BECKN_ONIX_DIR"
+        __besman_echo_yellow "Removed beckn-onix directory"
+    fi
+
+    # remove oasp-seller-app dir
+    if [[ -d $BESMAN_OASP_SELLER_DIR ]]; then
+        rm -rf "$BESMAN_OASP_SELLER_DIR"
+        __besman_echo_yellow "Removed oasp-seller-app directory"
     fi
 }
 
