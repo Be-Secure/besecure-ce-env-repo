92874a67a7b5375032457a0b03ff7ee21ea2cd10
NeerajK007 <neeraj.kumar32@wipro.com>
2025-03-12 17:51:19 +0530

    updated env with security assessment tool

--- shell_gpt/0.0.1/besman-shell_gpt-RT-env.sh
+++ shell_gpt/0.0.1/besman-shell_gpt-RT-env.sh
@@ -3,7 +3,7 @@
 ## Lifecycle Functions
 
 __besman_install() {
-    __besman_echo_white "Starting installation of ShellGPT environment..."
+    __besman_echo_white "Installing environment for $BESMAN_ENV_NAME..."
 
     # Default Besman installation steps
     __besman_check_vcs_exist || return 1 # Checks if GitHub CLI is present.
@@ -132,11 +132,57 @@ EOL
 
     __besman_echo_white "ShellGPT configuration updated successfully."
 
+    # Install Assessment Tools
+    [[ ! -z $BESMAN_ASSESSMENT_TOOLS ]] && readarray -d ',' -t ASSESSMENT_TOOLS <<<"$BESMAN_ASSESSMENT_TOOLS"
+
+    if [ ! -z $ASSESSMENT_TOOLS ]; then
+        for tool in ${ASSESSMENT_TOOLS[*]}; do
+            if [[ $tool == *:* ]]; then
+                tool_name=${tool%%:*}
+                tool_version=${tool##*:}
+            else
+                tool_name=$tool
+                tool_version=""
+            fi
+
+            __besman_echo_white "Installing tool - $tool_name : version - $tool_version"
+
+            case $tool_name in
+            criticality_score)
+                if ! [ -x "$(command -v criticality_score)" ]; then
+                    go install github.com/ossf/criticality_score/v2/cmd/criticality_score@latest
+                fi
+                ;;
+            sonarqube)
+                docker ps -aq -f name=sonarqube-$BESMAN_ARTIFACT_NAME && docker stop sonarqube-$BESMAN_ARTIFACT_NAME && docker rm -f sonarqube-$BESMAN_ARTIFACT_NAME
+                docker create --name sonarqube-$BESMAN_ARTIFACT_NAME -p 9000:9000 sonarqube && docker start sonarqube-$BESMAN_ARTIFACT_NAME
+                ;;
+            fossology)
+                docker ps -aq -f name=fossology-$BESMAN_ARTIFACT_NAME && docker stop fossology-$BESMAN_ARTIFACT_NAME && docker rm -f fossology-$BESMAN_ARTIFACT_NAME
+                docker create --name fossology-$BESMAN_ARTIFACT_NAME -p 8081:80 fossology/fossology && docker start fossology-$BESMAN_ARTIFACT_NAME
+                ;;
+            spdx-sbom-generator)
+                curl -L -o $BESMAN_ARTIFACT_DIR/spdx-sbom-generator.tar.gz "$BESMAN_SPDX_SBOM_ASSET_URL"
+                tar -xzf $BESMAN_ARTIFACT_DIR/spdx-sbom-generator.tar.gz -C $BESMAN_ARTIFACT_DIR
+                ;;
+            cyclonedx-sbom-generator)
+                if ! which cdxgen >/dev/null; then
+                    sudo npm install -g @cyclonedx/cdxgen && sudo cp /usr/bin/cdxgen /opt/cyclonedx-sbom-generator
+                fi
+                ;;
+            *)
+                __besman_echo_white "No installation steps found for $tool_name."
+                ;;
+            esac
+        done
+        __besman_echo_white "Bes-Assessment tools installation completed."
+    fi
+
     __besman_echo_white "Installation complete."
 }
 
 __besman_uninstall() {
-    __besman_echo_white "Starting uninstallation..."
+    __besman_echo_white "Uninstalling environment for $BESMAN_ENV_NAME..."
 
     if [[ -d $BESMAN_ARTIFACT_DIR ]]; then
         __besman_echo_white "Removing $BESMAN_ARTIFACT_DIR..."
@@ -168,35 +214,58 @@ __besman_uninstall() {
         rm -rf "$HOME/sgpt_env"
     fi
 
-    # # Remove Ollama model
-    # __besman_echo_white "Checking for Ollama model: mistral:7b-instruct..."
-    # if ollama list | grep -q "mistral:7b-instruct"; then
-    #     __besman_echo_white "Removing Ollama model mistral:7b-instruct..."
-    #     ollama rm mistral:7b-instruct
-    # else
-    #     __besman_echo_white "Model mistral:7b-instruct not found. Skipping..."
-    # fi
+    # Remove Ollama model
+    __besman_echo_white "Checking for Ollama model: mistral:7b-instruct..."
+    if ollama list | grep -q "mistral:7b-instruct"; then
+        __besman_echo_white "Removing Ollama model mistral:7b-instruct..."
+        ollama rm mistral:7b-instruct
+    else
+        __besman_echo_white "Model mistral:7b-instruct not found. Skipping..."
+    fi
 
-    # # Remove Ollama
-    # if command -v ollama &>/dev/null; then
-    #     __besman_echo_white "Uninstalling Ollama..."
+    # Remove Ollama
+    if command -v ollama &>/dev/null; then
+        __besman_echo_white "Uninstalling Ollama..."
 
-    #     # Stop and disable the service before deleting files
-    #     sudo systemctl stop ollama
-    #     sudo systemctl disable ollama
+        # Stop and disable the service before deleting files
+        sudo systemctl stop ollama
+        sudo systemctl disable ollama
 
-    #     # Remove Ollama binaries and cached models
-    #     sudo rm -rf /usr/local/bin/ollama
-    #     sudo rm -rf ~/.ollama
-    # else
-    #     __besman_echo_white "Ollama is not installed. Skipping..."
-    # fi
+        # Remove Ollama binaries and cached models
+        sudo rm -rf /usr/local/bin/ollama
+        sudo rm -rf ~/.ollama
+    else
+        __besman_echo_white "Ollama is not installed. Skipping..."
+    fi
+
+    # Remove installed assessment tools
+    [[ ! -z $BESMAN_ASSESSMENT_TOOLS ]] && readarray -d ',' -t ASSESSMENT_TOOLS <<<"$BESMAN_ASSESSMENT_TOOLS"
+
+    for tool in ${ASSESSMENT_TOOLS[*]}; do
+        case $tool in
+        sonarqube)
+            docker stop sonarqube-$BESMAN_ARTIFACT_NAME && docker rm -f sonarqube-$BESMAN_ARTIFACT_NAME
+            ;;
+        fossology)
+            docker stop fossology-$BESMAN_ARTIFACT_NAME && docker rm -f fossology-$BESMAN_ARTIFACT_NAME
+            ;;
+        criticality_score)
+            rm -rf $(which criticality_score)
+            ;;
+        spdx-sbom-generator)
+            rm -rf $BESMAN_ARTIFACT_DIR/spdx-sbom-generator*
+            ;;
+        cyclonedx-sbom-generator)
+            sudo npm uninstall -g @cyclonedx/cdxgen
+            ;;
+        esac
+    done
 
     __besman_echo_white "Uninstallation complete."
 }
 
 __besman_validate() {
-    __besman_echo_white "Validating environment..."
+    __besman_echo_white "Validating environment for $BESMAN_ENV_NAME..."
     local available_deps=()
     local missing_deps=()
 
@@ -229,6 +298,29 @@ __besman_validate() {
     __besman_echo_white "Available dependencies: ${available_deps[*]}"
     __besman_echo_yellow "Missing dependencies: ${missing_deps[*]}"
 
+    # Validate installed assessment tools
+    [[ ! -z $BESMAN_ASSESSMENT_TOOLS ]] && readarray -d ',' -t ASSESSMENT_TOOLS <<<"$BESMAN_ASSESSMENT_TOOLS"
+
+    for tool in ${ASSESSMENT_TOOLS[*]}; do
+        case $tool in
+        criticality_score)
+            command -v criticality_score >/dev/null && __besman_echo_white "criticality_score: Installed" || __besman_echo_white "criticality_score: Missing"
+            ;;
+        sonarqube)
+            docker ps -a --format "{{.Names}}" | grep -q sonarqube-$BESMAN_ARTIFACT_NAME && __besman_echo_white "sonarqube: Installed" || __besman_echo_white "sonarqube: Missing"
+            ;;
+        fossology)
+            docker ps -a --format "{{.Names}}" | grep -q fossology-$BESMAN_ARTIFACT_NAME && __besman_echo_white "fossology: Installed" || __besman_echo_white "fossology: Missing"
+            ;;
+        spdx-sbom-generator)
+            [[ -f $BESMAN_ARTIFACT_DIR/spdx-sbom-generator ]] && __besman_echo_white "spdx-sbom-generator: Installed" || __besman_echo_white "spdx-sbom-generator: Missing"
+            ;;
+        cyclonedx-sbom-generator)
+            command -v cdxgen >/dev/null && __besman_echo_white "cyclonedx-sbom-generator: Installed" || __besman_echo_white "cyclonedx-sbom-generator: Missing"
+            ;;
+        esac
+    done
+
     if [[ ${#missing_deps[@]} -ne 0 ]]; then
         return 1
     fi
@@ -237,17 +329,14 @@ __besman_validate() {
 }
 
 __besman_update() {
-    __besman_echo_white "Updating ShellGPT with LiteLLM..."
-    if command -v sgpt &>/dev/null; then
-        pip install --upgrade "shell-gpt[litellm]"
-        __besman_echo_white "Update complete."
-    else
-        __besman_echo_white "ShellGPT not found. Please install first."
-    fi
+    __besman_echo_white "Updating environment for $BESMAN_ENV_NAME..."
+    # Reinstall tools if versions need to be updated
+    __besman_uninstall
+    __besman_install
 }
 
 __besman_reset() {
-    __besman_echo_white "Resetting environment..."
+    __besman_echo_white "Resetting environment for $BESMAN_ENV_NAME..."
     __besman_uninstall
     __besman_install
     __besman_echo_white "Reset complete."
